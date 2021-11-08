353eca2c980cd76df2f8d3a41652494b891e10e4
Melli <bennetcx@gmail.com>
2021-11-08 20:37:58 +0100

    Refactor

--- src/main/java/de/rapshansa/FSDAircraftSim.java
+++ src/main/java/de/rapshansa/FSDAircraftSim.java
@@ -1,11 +1,15 @@
 package de.rapshansa;
 
+import de.rapshansa.packets.FSDAddPilotPacket;
+
+import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class FSDAircraftSim {
-    public static void main(String[] args) throws MalformedURLException {
-        FSDClient fsdClient = new FSDClient("", "", new InetSocketAddress("127.0.0.1", 6809));
+    public static void main(String[] args) throws IOException {
+        FSDClient fsdClient = new FSDClient("123456", "EinPasswort", new InetSocketAddress("127.0.0.1", 6809));
+        fsdClient.connect();
+        String[] params = {"RAH69M", "SERVER", "Melli", "111111", "password", "1", "9", "0"};
+        fsdClient.sendPacket(new FSDAddPilotPacket(params));
     }
 }
