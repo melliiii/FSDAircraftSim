010085863cd68ef90f520891045eacf02b3120d6
Melli <bennetcx@gmail.com>
2021-11-08 22:01:21 +0100

    Refactor

---
+++ src/main/java/de/rapshansa/packets/FSDSendFlightPlan.java
@@ -0,0 +1,48 @@
+package de.rapshansa.packets;
+
+/**
+ * Adds a pilot to the server / login
+ * @author Melli
+ * @version 1.0
+ * @see FSDPacket
+ */
+public class FSDSendFlightPlan implements FSDPacket{
+
+    static final String command = "$FP";
+    private String[] params;
+
+    /**
+     * Creates Packet with params
+     * @param params Fields: MSG FROM, MSG TO, CERTIFICATE ID ,PASSWORD, RATING (num between 0 and 5), PROTOCOL REVISION LETTER, SIM LETTER
+     */
+    public FSDSendFlightPlan(String[] params){
+        this.params = params;
+    }
+
+    /**
+     * Sets params for packet
+     * @param params Fields: MSG FROM, MSG TO, CERTIFICATE ID ,PASSWORD, RATING (num between 0 and 5), PROTOCOL REVISION LETTER, SIM LETTER
+     */
+    @Override
+    public FSDPacket setParams(String[] params) {
+        this.params = params;
+        return this;
+    }
+
+    /**
+     * Creates a packet
+     * @return packet as string
+     */
+    @Override
+    public String asString() {
+        StringBuilder line = new StringBuilder(command);
+        for(int i = 0; i < params.length; i++){
+            if(i != 0){
+                line.append(":");
+            }
+            line.append(params[i]);
+        }
+        line.append("\r\n");
+        return line.toString();
+    }
+}
